de0f2db5c247e04cb1673e4c7f707530eca46883
Kimsunghyun <sorkaj123@gmail.com>
2021-05-11 16:49:00 +0900

    Add files via upload

---
+++ HTTPComm/HTTPWebComm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace HTTPComm
+{
+    public class HTTPWebComm
+    {
+        string URL;
+        string message;
+        string resultStr;
+
+        HttpWebRequest request;
+
+        public void SetURL(string url)
+        {
+            URL = url;
+        }
+
+        public void setMessage(string msg)
+        {
+            message = msg;
+        }
+
+        public void Reqeust()
+        {
+            request = (HttpWebRequest)WebRequest.Create(URL);
+            byte[] sendData =UTF8Encoding.UTF8.GetBytes(message);
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            request.Method = "POST";
+            StreamWriter sw = new StreamWriter(request.GetRequestStream());
+
+
+            sw.Write(message);
+            sw.Close();
+        }
+
+        public string Response()
+        {
+            HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+            resultStr = streamReader.ReadToEnd();
+
+            streamReader.Close();
+            httpWebResponse.Close();
+            return resultStr;
+
+        }
+    }
+}
